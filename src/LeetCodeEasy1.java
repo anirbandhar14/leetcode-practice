8125be9b09186c0a888a018967798f153cfb6748
Anirban Dhar <anirbandhar@Anirbans-MacBook-Air.local>
2026-04-21 21:45:42 -0400

    sum position target

---
+++ src/LeetCodeEasy1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LeetCodeEasy1 {
+
+    public static void main(String[] args) {
+        int[] nums = {-5,3,2,4,5};
+        int target = 0;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums.length < 2 || nums.length > Math.pow(10, 4) ||
+        target < Math.pow(-10, 9) || target > Math.pow(10, 9)) {
+            throw new RuntimeException("Invalid input");
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new RuntimeException("Invalid input");
+    }
+}
